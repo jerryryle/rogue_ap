1a2d9b8bd8b11207bbb8ed891a5a1310901bc136
Jerry Ryle <jerryryle@gmail.com>
2020-05-10 11:01:46 -0700

    Fixed bug that prevented scripts from being copied.

--- setup.sh
+++ setup.sh
@@ -39,8 +39,8 @@ copy_with_backup () {
 	# If the destination file exists, back it up--but only if the backup file does not already exist
 	if [ -f $2 ] && [ ! -f $2.rogueap-old ]; then
 		mv -f ${2} ${2}.rogueap-old
-		cp -f ${1} ${2}
 	fi
+	cp -f ${1} ${2}
 }
 
 echo "Copying config files..."
